6eb9f31171a47c359c4b11a3f0813ed8a449fc38
Federica <federica.manograssi@gmail.com>
2021-04-07 12:56:10 +0200

    CONSEGNA MILESTONE 2

--- js/main.js
+++ js/main.js
@@ -8,8 +8,9 @@ var app= new Vue({
     },
     methods : {
         userResearch(){
-            // let searchedMovies= [];
-            // let searchedSeries = [];
+            let searchedMovies= [];
+            let searchedSeries = [];
+
             // RICERCA TRA I MOVIES---------------------------------
             axios
             // .get('https://api.themoviedb.org/3/search/movie?api_key=a4ea26af321c4cf77360180c2fb6b66c&query='+this.searchbar)
@@ -19,13 +20,9 @@ var app= new Vue({
                         query : this.searchbar
                     }})
                 .then((searchedMovieList)=>{
-                    // console.log(searchedMovieList.data.results)
-                    this.userResearchResults = searchedMovieList.data.results;
-                    // console.log(this.searchedMovies)
 
-                    // tentativo concat
-                    // searchedMovies = searchedMovieList.data.results;
-                    // console.log(searchedMovies)
+                    searchedMovies = searchedMovieList.data.results;
+                    this.userResearchResults = this.userResearchResults.concat(searchedMovies)
                 });
 
             // RICERCA TRA LE SERIES---------------------------
@@ -36,12 +33,11 @@ var app= new Vue({
                         query: this.searchbar
                     }})
                 .then((searchedSeriesList)=>{
-                    // console.log(searchedSeriesList);
-
-                    // tentativo concat
-                    // searchedSeries = searchedSeriesList.data.results;
-                    // console.log(searchedSeries)
+                    console.log(searchedSeriesList)
+                    searchedSeries = searchedSeriesList.data.results;
+                    this.userResearchResults= this.userResearchResults.concat(searchedSeries)
                 });
+            
         },
         starVote(vote_average){
             return Math.ceil(vote_average * this.starNumber / 10)
